8cec8c50985516195593c4f30635773ecaa4eba6
Jacobo Ruiz <jacobo.ruiz@universite-paris-saclay.fr>
2021-04-29 19:10:27 +0200

    Marshall and Bounties on board of the train! UPDATE

---
+++ src/engine/gameElements/Marshall.java
@@ -0,0 +1,12 @@
+package engine.gameElements;
+
+public class Marshall extends Character {
+
+    public Marshall(Train t, int abs){
+        super("Marshall",t, abs, 1);
+    }
+    //TODO
+    public void autoMove(){}
+    //TODO
+    public void catchBandit(){}
+}
