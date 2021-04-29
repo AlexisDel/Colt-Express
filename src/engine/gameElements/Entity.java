dac0b6c5b76acdf6c83616b40d20ccb96da7d5d2
Jacobo Ruiz <jacobo.ruiz@universite-paris-saclay.fr>
2021-04-29 19:09:47 +0200

    Marshall and Bounties on board of the train! UPDATE

--- src/engine/gameElements/Entity.java
+++ src/engine/gameElements/Entity.java
@@ -3,12 +3,15 @@ package engine.gameElements;
 public class Entity {
     protected int x,y;
     Train train;
+    private String id;
 
-    public Entity(Train t, int abs, int ord){
+    public Entity(String id, Train t, int abs, int ord){
+        this.id=id;
         this.x = abs;
         this.y = ord;
         this.train = t;
     }
+    public String getID(){return this.id; }
 
     public int getX() {
         return this.x;
