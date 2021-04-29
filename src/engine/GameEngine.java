a171c96e17f06f9538b5cb2ac447ab90dd69b6e4
Jacobo Ruiz <jacobo.ruiz@universite-paris-saclay.fr>
2021-04-29 19:11:01 +0200

    Marshall and Bounties on board of the train! UPDATE

--- src/engine/GameEngine.java
+++ src/engine/GameEngine.java
@@ -2,9 +2,13 @@ package engine;
 
 import controller.GameController;
 import engine.gameElements.Bandit;
+import engine.gameElements.Bounty;
+import engine.gameElements.Marshall;
 import engine.gameElements.Train;
 import engine.utils.GameState;
 import graphics.GameDisplay;
+import java.util.concurrent.ThreadLocalRandom;
+
 
 public class GameEngine {
 
@@ -15,7 +19,7 @@ public class GameEngine {
     public GameState gameState;
 
     public Train train;
-    private Bandit CrocoMechant;
+    private Bandit[] bandits;
 
     public GameEngine() {
 
@@ -23,16 +27,59 @@ public class GameEngine {
         this.gameState = GameState.PLANNING;
 
         this.train = new Train();
-        this.CrocoMechant = new Bandit("CrocoMechant", this.train, 0,0);
+        //Adds players in the String list, setups the marshall and the bounty
+        String[] players= new String[]{"CrocoMechant","Fantiks"};
+        bandits= new Bandit[players.length];
+        setupEntities(players);
 
         this.gameController = new GameController(this);
         this.gameDisplay = new GameDisplay(this);
 
     }
+    public void setupEntities(String[] playerNames){
+        //Setup players
+        for(int i=0; i<playerNames.length;i++){
+            Bandit player = new Bandit(playerNames[i] , this.train, 0,0);
+            this.train.addEntity(player);
+            this.bandits[i]= player;
+
+        }
+        //Spawns the marshall
+        Marshall marshall= new Marshall(this.train, this.train.getTrainLength()-1);
+        this.train.addEntity(marshall);
+        //Spawns the bounty
+        genBounty();
+
+    }
+    public void genBounty(){
+        //Locomotive bounty
+        Bounty treasure= new Bounty(this.train, train.getTrainLength()-1, "Treasure",1000);
+        this.train.addEntity(treasure);
+        //Other bounty
+        int randomNum1=0;
+        int randomNum2=0;
+        int randValue=0;
+        //For each wagon
+        for(int x=0; x< train.getTrainLength()-1;x++){
+             randomNum1= ThreadLocalRandom.current().nextInt(1, 4 + 1);
+             //Random num of jewelry and bags
+            for(int k=0; k< randomNum1;k++){
+                randValue=ThreadLocalRandom.current().nextInt(0, 500 + 1);
+                Bounty bag= new Bounty(this.train, x, "Bag",randValue);
+                this.train.addEntity(bag);
+            } randomNum2=ThreadLocalRandom.current().nextInt(1, 4 + 1);
+            for(int k=0; k< randomNum2-randomNum1;k++){
+                Bounty jewelry= new Bounty(this.train, x, "Jewel",500);
+                this.train.addEntity(jewelry);
+            }
+        }
+    }
 
     public void update() {
         if (this.gameState == GameState.ACTION){
-            CrocoMechant.update(gameController.actions);
+            //Croco bandit selected
+            //TODO: Enable more players (Changes needed elsewhere)
+            bandits[0].update(gameController.actions);
             gameController.resetActionsQueue();
             gameState = GameState.PLANNING;
         }
