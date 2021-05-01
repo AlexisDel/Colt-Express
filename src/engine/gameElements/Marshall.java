f853ee7e8c67ed0857425e23a4cfd86b4f88e5bb
Jacobo Ruiz <jacobo.ruiz@universite-paris-saclay.fr>
2021-05-01 17:12:01 +0200

    moving marshall version

--- src/engine/gameElements/Marshall.java
+++ src/engine/gameElements/Marshall.java
@@ -1,12 +1,40 @@
 package engine.gameElements;
 
+import engine.utils.Direction;
+
+import java.util.Random;
+
 public class Marshall extends Character {
+    private float NERVOSITE_MARSHALL;
 
     public Marshall(Train t, int abs){
         super("Marshall",t, abs, 1);
+        this.NERVOSITE_MARSHALL= (float) 0.3;
     }
     //TODO
-    public void autoMove(){}
+    public void autoMove(){
+        Random r= new Random();
+        int probability= r.nextInt(100);
+        boolean randomDirection= r.nextBoolean();
+        if(probability<=NERVOSITE_MARSHALL*100) {
+           if(randomDirection){this.move(Direction.LEFT);}
+           else{this.move(Direction.RIGHT);}
+        }
+    }
     //TODO
-    public void catchBandit(){}
+    public void catchBandit(){
+        for(Bandit b: this.train.getBandits()){
+            if(this.x==b.getX()&& b.getY()==1){
+                //TODO: drops 1 random bounty
+                b.move(Direction.UP);
+                System.out.println("The Marshall caught "+b.getID());
+            }
+        }
+    }
+
+    public void update(){
+        catchBandit();
+        autoMove();
+        catchBandit();
+    }
 }
