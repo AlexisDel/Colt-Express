a6336770b39ff031c9251a122df863273c1d3802
Jacobo Ruiz <jacobo.ruiz@universite-paris-saclay.fr>
2021-05-01 17:12:13 +0200

    moving marshall version

--- src/engine/gameElements/Train.java
+++ src/engine/gameElements/Train.java
@@ -1,6 +1,7 @@
 package engine.gameElements;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Train {
 
@@ -21,6 +22,25 @@ public class Train {
         return this.entities;
     }
 
+    public List<Bandit> getBandits(){
+        List<Bandit>res= new ArrayList<>();
+        for (Entity e: this.getEntities()){
+            if (e instanceof Bandit){
+                res.add((Bandit) e);
+            }
+        }
+        return res;
+    }
+
+    public Marshall getMarshall(){
+        Marshall res= new Marshall(this,0);
+        for (Entity e: this.getEntities()) {
+            if (e instanceof Marshall) {
+                res= (Marshall) e;
+            }
+        }
+        return res;
+    }
 
     public int getTrainLength(){
         return NB_WAGONS;
