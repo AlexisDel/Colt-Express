7e9cb70a3076dd84b2691917fd90601eae2aa139
Jacobo Ruiz <jacobo.ruiz@universite-paris-saclay.fr>
2021-05-01 17:09:16 +0200

    moving marshall version

--- src/engine/gameElements/Bandit.java
+++ src/engine/gameElements/Bandit.java
@@ -4,19 +4,28 @@ import engine.utils.Action;
 import engine.utils.Direction;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Bandit extends Character {
     final String NOM_BANDIT;
-    Train train;
-    ArrayList<Bounty> bounties;
-    Action[] actions;
+    private Train train;
+    private ArrayList<Bounty> bounties;
+    private List <Action> actions;
+    private boolean actionsCompleted;
 
     public Bandit(String name, Train t, int abs, int ord) {
         super(name,t, abs, ord);
         this.NOM_BANDIT = name;
         this.bounties = new ArrayList<>();
-        this.actions = new Action[2];
+        this.actions= new ArrayList<>();
+        //actions list init as empty so all actions completed
+        this.actionsCompleted=true;
+
     }
+
+    public boolean getActionsCompleted(){return this.actionsCompleted;}
+    public List<Action> getActions(){return this.actions;}
+
     //TODO
     public void rob(){}
 
@@ -24,16 +33,16 @@ public class Bandit extends Character {
         //TODO: Drop random bounty method, finish this method
         switch (d) {
             case LEFT -> {
-                System.out.println( "motherfucker shoot left");
+                System.out.println( this.getID()+" shoot left");
             }
             case RIGHT -> {
-                System.out.println( "motherfucker shoot right");
+                System.out.println( this.getID()+" shoot right");
             }
             case UP -> {
-                System.out.println( "motherfucker shoot da ceiling");
+                System.out.println( this.getID()+" shoot da ceiling");
             }
             case DOWN -> {
-                System.out.println( "motherfucker shot da train");
+                System.out.println( this.getID()+" shot da train");
             }
 
         }
@@ -41,8 +50,8 @@ public class Bandit extends Character {
     //TODO
     public void getMoney(){}
 
-    public void doActions(){
-        for(Action a: actions){
+    public void doAction(Action a){
+
             switch (a) {
                 case MOVE_UP -> { this.move(Direction.UP);
                 }
@@ -63,14 +72,29 @@ public class Bandit extends Character {
                 case ROB -> { this.rob();
                 }
 
-            }
+
         }
     }
-    //TODO: throw exception if any actions are null
-    public void update(Action[] actions){
 
-        this.actions = actions;
-        doActions();
+    public void setActionTo(List<Action>a){
+        this.actions=a;
+        this.actionsCompleted = false;
+    }
+
+
+
+
+    //TODO: throw exception if any actions are null
+    public void update(){
+        //if the list is not empty
+        if(this.actions.size()>0) {
+            //do last action in queue
+            doAction(this.actions.get(0));
+            //remove last action in queue
+            this.actions.remove(0);
+        }
+        //if the list is empty, all actions should have been executed
+        else{ this.actionsCompleted=true;}
     }
 
 }
